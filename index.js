fbe87ffc89d6b8176756a12834eab1d4e020a75a
37126fs1d8 <wrf@45>
2025-12-20 16:53:58 +0100

    refactor: undo and add health endpoint

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'views', 'index.html'));
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok', port: PORT });
 });
 
 app.use('/api/fortune', fortuneRoutes);
